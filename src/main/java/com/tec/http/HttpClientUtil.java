2d7d641bbcad2c765b8dc9934c9c4e6834c76336
jasonsun1985 <jasonsun1985@sohu.com>
2019-12-13 14:53:38 +0800

    update

---
+++ src/main/java/com/tec/http/HttpClientUtil.java
@@ -0,0 +1,112 @@
+package com.tec.http;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+/*
+* 利用HttpClient进行post请求的工具类
+*/
+public class HttpClientUtil {
+	public static String doPost(String url, Map<String, String> map, String charset) {
+		HttpClient httpClient = null;
+		HttpPost httpPost = null;
+		String result = null;
+		try {
+			httpClient = new SSLClient();
+			httpPost = new HttpPost(url);
+			// 设置参数
+			List<NameValuePair> list = new ArrayList<NameValuePair>();
+			Iterator iterator = map.entrySet().iterator();
+			while (iterator.hasNext()) {
+				Entry<String, String> elem = (Entry<String, String>) iterator.next();
+				list.add(new BasicNameValuePair(elem.getKey(), elem.getValue()));
+			}
+			if (list.size() > 0) {
+				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, charset);
+				httpPost.setEntity(entity);
+			}
+			HttpResponse response = httpClient.execute(httpPost);
+			if (response != null) {
+				HttpEntity resEntity = response.getEntity();
+				if (resEntity != null) {
+					result = EntityUtils.toString(resEntity, charset);
+				}
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+			return "888888";/** 链接失败 */
+		}
+		return result;
+	}
+
+	public String doGet(String url, Map<String, String> param) {
+
+		// 创建Httpclient对象
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+
+		String resultString = "";
+		CloseableHttpResponse response = null;
+		try {
+			// 创建uri
+			URIBuilder builder = new URIBuilder(url);
+			if (param != null) {
+				for (String key : param.keySet()) {
+					builder.addParameter(key, param.get(key));
+				}
+			}
+			java.net.URI uri = builder.build();
+
+			// 创建http GET请求
+			HttpGet httpGet = new HttpGet(uri);
+
+			// 执行请求
+			response = httpclient.execute(httpGet);
+			// 判断返回状态是否为200
+			if (response.getStatusLine().getStatusCode() == 200) {
+				resultString = EntityUtils.toString(response.getEntity(), "iso8859-1");
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (response != null) {
+					response.close();
+				}
+				httpclient.close();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return resultString;
+	}
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("username", "riil");
+		map.put("password", "riiladmin");
+		
+		System.out.println(doPost("https://172.17.160.40:8443", map, "utf-8"));
+	}
+}
