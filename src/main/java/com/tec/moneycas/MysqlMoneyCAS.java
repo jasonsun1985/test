7228ba2998a3e59cd367abdb3a06f27189f4a9cb
jasonsun1985 <jasonsun1985@sohu.com>
2019-12-24 14:42:19 +0800

    update

--- src/main/java/com/tec/moneycas/MysqlMoneyCAS.java
+++ src/main/java/com/tec/moneycas/MysqlMoneyCAS.java
@@ -4,28 +4,30 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.locks.ReentrantLock;
 
 import com.alibaba.druid.pool.DruidDataSource;
 
-/** 
- * @Description:
- * 为了保证扣款不会出现多扣 余额为负数。
- * 1.查询当前账户余额。
- * 2.更新账户余额时候 使用id和上面查询的余额作为唯一条件去扣款。
- * 3.扣款结果为0说明没成功，为1说明成功更新了一条。
- * <p>创建日期：2019年12月24日 </p>
- * @version V1.0  
+/**
+ * @Description: 为了保证扣款不会出现多扣 余额为负数。 1.查询当前账户余额。 2.更新账户余额时候 使用id和 (当前账户余额-消费金额)大于等于0
+ * 作为唯一条件去扣款。 3.扣款结果为0说明没成功，为1说明成功更新了一条。
+ * 
+ * 场景： 账户有130元，每个线程扣1块，3000个线程并发扣款不会出现负数，最后结果为1
+ * <p>
+ * 创建日期：2019年12月24日
+ * </p>
  * @author SUNLEI
- * @see
  */
 public class MysqlMoneyCAS {
 	private static DruidDataSource druidDataSource = new DruidDataSource(); // 阿里的连接池
 	private static Connection connection;
+
 	private MysqlMoneyCAS() {
 		if (connection == null) {
 			connection = getConnection(druidDataSource); // 连接池
 		}
 	}
+
 	private static Connection getConnection(DruidDataSource druidDataSource) {
 		druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		// 注意，替换成自己本地的 mysql 数据库地址和用户名、密码
@@ -49,36 +51,63 @@ public class MysqlMoneyCAS {
 		}
 		return con;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		MysqlMoneyCAS moneyCAS = new MysqlMoneyCAS();
-		try {
-			PreparedStatement psR = connection.prepareStatement("SELECT money FROM money WHERE id = 1");
-			ResultSet rs = psR.executeQuery();
-			int money = 0;
-			while (rs.next()) {
-				money = rs.getInt(1);
-				System.out.println("money : " + money);
-			}
-			for (int i = 0; i < 10; i++) {
-				new Thread(()-> {
-					try {
-						PreparedStatement ps = connection.prepareStatement("update money set money=? - 20 where id = ? and money = 20");
-						ps.setInt(1, 20);
-						ps.setString(2, "1");
-						int updateRs = ps.executeUpdate();
-						System.out.println("扣减结果： " + updateRs);
+		consumeCAS();
+		produceCAS();
+		
+	}
+
+	private static void produceCAS() {
+		final int produce = 1;
+		for (int i = 0; i < 2000; i++) {
+			new Thread(() -> {
+				try {
+					PreparedStatement psR = connection.prepareStatement("SELECT money FROM money WHERE id = 1");
+					ResultSet rs = psR.executeQuery();
+					int money = 0;
+					while (rs.next()) {
+						money = rs.getInt(1);
+						System.out.println("账户余额 money : " + money);
 					}
-					catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}).start();
-			}
+					PreparedStatement ps = connection.prepareStatement("update money set money=money + " + produce + " where id = ? ");
+					// ps.setInt(1, money);
+					ps.setString(1, "1");
+//					ps.setInt(2, money);
+					int updateRs = ps.executeUpdate();
+					System.out.println("充值结果： " + updateRs);
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}).start();
 		}
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	}
+
+	private static void consumeCAS() {
+		final int consume = 3;
+		for (int i = 0; i < 3000; i++) {
+			new Thread(() -> {
+				try {
+					PreparedStatement psR = connection.prepareStatement("SELECT money FROM money WHERE id = 1");
+					ResultSet rs = psR.executeQuery();
+					int money = 0;
+					while (rs.next()) {
+						money = rs.getInt(1);
+						System.out.println("账户余额 money : " + money);
+					}
+					PreparedStatement ps = connection.prepareStatement("update money set money=money - " + consume + " where id = ? and money = ? and money - " + consume + " >=0");
+					// ps.setInt(1, money);
+					ps.setString(1, "1");
+					ps.setInt(2, money);
+					int updateRs = ps.executeUpdate();
+					System.out.println("扣减结果： " + updateRs);
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}).start();
 		}
 	}
 }
